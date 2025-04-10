a4a005853d872de36e9074aafa228973933120ba
Karsa.Wang <wanghongtao114@gmail.com>
2025-04-10 20:53:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 env GOOS=linux GOARCH=amd64 go build -o linux_amd64 main.go;
 env GOOS=darwin GOARCH=amd64 go build -o mac_amd64 main.go;
+env GOOS=darwin GOARCH=arm64 go build -o mac_arm64 main.go;
